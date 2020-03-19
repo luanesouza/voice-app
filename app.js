9afda6acb6b2ebf09e430db74e09dcfbd5c5df90
luanesouza <luanesouza411@gmail.com>
2020-03-19 08:33:18 -0400

    app responds to voice commands

--- app.js
+++ app.js
@@ -18,9 +18,27 @@ recognition.onresult = (event) => {
   const currentResultIndex = event.resultIndex;
   const transcript = event.results[currentResultIndex][0].transcript;
   console.log(transcript);
+  readOutLoud(transcript)
 }
 
 talkButton.addEventListener('click', () => {
   console.log('button clicked');
   recognition.start();
 })
+
+const readOutLoud = (message) => {
+  const speech = new SpeechSynthesisUtterance
+  speech.text = personalizedAnswers(message);
+  speech.volume = 3;
+  speech.pitch = 1;
+  speech.rate = 1;
+
+  window.speechSynthesis.speak(speech)
+}
+
+const personalizedAnswers = (message) => {
+  let arrayOfGreetings = ['I am well, what do you want to talk about today?', 'It is a nice day out. What are you doing talking to me? Go for a walk.']
+  let randomIndex = Math.floor(Math.random() * Math.floor(arrayOfGreetings.length))
+  console.log(randomIndex);
+  return arrayOfGreetings[randomIndex];
+}
